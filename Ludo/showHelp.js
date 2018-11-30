a80799e5d8bd68a4705921e8e99fe25923004b53
Thorabc <43777499+Thorabc@users.noreply.github.com>
2018-11-30 20:42:38 +0100

    Delete showHelp.js

--- Ludo/showHelp.js
+++
@@ -1,6 +0,0 @@
-function showHelp() {
-    document.getElementById('board').innerHTML = `
-        <h3>Spilleregler:</h3> <h4>Enkel</h4> Kast terningen og si et ord på den bokstaven som brikken din havner på. <h4>Medium</h4> Kast terningen og si et ord som slutter på den bokstaven brikken din lander på. <h4>Vanskelig</h4> Kast terningen og si et ord i den kategorien som bildet brikken lander på viser (er det bilde av et dyr, si navnet på et dyr)
-        `;
-}
-
